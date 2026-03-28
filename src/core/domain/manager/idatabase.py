b3b374319161e22169abfe232eb4a352948b887a
chamika <chamika@softvilmedia.com>
2026-03-28 21:29:47 +0530

    added sql ops

--- src/core/domain/manager/idatabase.py
+++ src/core/domain/manager/idatabase.py
@@ -2,17 +2,26 @@
 # Created: 2026-03-28
 
 from abc import ABC, abstractmethod
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Optional
+
 
 class IDatabase(ABC):
     """
-    For SQL Database
+    RDS SQL operations
     """
 
     @abstractmethod
+    def create_table(self, table_name: str, columns: Dict[str, str]) -> bool:
+        pass
+
+    @abstractmethod
+    def table_exists(self, table_name: str) -> bool:
+        pass            
+
+    @abstractmethod
     def insert_records(self, table_name: str, records: List[Dict[str, Any]]) -> bool:
-        """
-        Inserts dictionary to a table
-        Retrn the status
-        """
+        pass
+
+    @abstractmethod
+    def query_records(self, table_name: str, filters: Optional[Dict[str, Any]] = None) -> List[Dict[str, Any]]:
         pass
