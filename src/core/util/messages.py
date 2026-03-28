8db3c121220284e890002cb2891e36b6cfc0609a
chamika <chamika@softvilmedia.com>
2026-03-28 12:33:22 +0530

    message file added for logs

---
+++ src/core/util/messages.py
@@ -0,0 +1,7 @@
+class LogMessages:
+   
+    # S3 Msgs
+    S3_UPLOAD_OK = "AWS S3 Upload OK: {path_key}"
+    S3_UPLOAD_ERROR = "AWS S3 Upload Error: {error}"
+    S3_DOWNLOAD_OK = "AWS S3 Download OK: {path_key}"
+    S3_DOWNLOAD_ERROR = "AWS S3 Download Error: {error}"
